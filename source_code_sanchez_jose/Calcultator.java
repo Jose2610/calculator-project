863a53ac5801377332bdec27dd658acad1efc97c
Jose Sanchez <jose2610nieves@gmail.com>
2021-10-22 12:15:21 -0700

    Modify String Inputs
    This update modifies given string math expressions and converts it into a array list to be converted from infix to RPN using shunting yard.

--- source_code_sanchez_jose/Calcultator.java
+++ source_code_sanchez_jose/Calcultator.java
@@ -120,20 +120,159 @@ public class Calcultator {
 
     // Main function for calculator
     public static void main(String[] args) {
-        //String given = "-5.78+-(4-2.23)+sin(0)*cos(1)/(1+tan(2*-ln(-3+2*(1.23+arcsin(1)))))";
+        String given = "-5.78+-(4-2.23)+sin(0)*cos(1)/(1+tan(2*-ln(-3+2*(1.23+arcsin(1)))))";
         //String test = "1.23+arcsin(1)";
         //List<String> expected = List.of("1", "2", "+", "3", "4", "/", "5", "6", "+", "^", "*");
-        //List<String> computed = createExpression(given);
-        //List<String> rpnComputed = shuntingYard(computed);
+        List<String> computed = createExpression(given);
+        List<String> rpnComputed = shuntingYard(computed);
 
         //System.out.println("Infix: " + given);
         //System.out.println("RPN (expected): " + expected);
 
-        //System.out.println("RPN (converted): " + computed);
-        //System.out.println("RPN (computed): " + rpnComputed);
+        System.out.println("RPN (converted): " + computed);
+        System.out.println("RPN (computed): " + rpnComputed);
 
         // Testing computeExpression
         //double result = computeExpression(rpnComputed);
         //System.out.println("RPN Solution: " + result);
-		System.exit(0);
     }
+
+
+    // createExpression function will take a string expression created by the user and translate
+    // it into a workable array list for the shunting yard algorithm
+    public static List<String> createExpression(String origin) {
+        char[] tempOrigin = origin.toCharArray();
+        List<Character> operators = Arrays.asList('+', '-', '/', '*', '(', ')', '^');
+        List<String> result = new ArrayList<>();
+
+        // Temparary variable to hold numbers
+        String number = "";
+
+        for (int i = 0; i < tempOrigin.length; i++) {
+            try {
+                // Checking for digits
+                if (Character.isDigit(tempOrigin[i]) || tempOrigin[i] == '.') {
+                    number += tempOrigin[i];
+                }
+
+                // Add digits into expression
+                else if (number.length() > 0) {
+                    result.add(number);
+                    number = "";
+                    i--;
+                }
+
+                // Check if token is contained in the operators list
+                else if (operators.contains(tempOrigin[i])) {
+                    // Check for unary subtraction symbol
+                    if (tempOrigin[i] == '-' && i == 0 && Character.isDigit(tempOrigin[i+1])) {
+                        number += "-"; 
+                    }
+                    // If unary negative symbol is not first, then try this one
+                    else if (tempOrigin[i] == '-' && operators.contains(tempOrigin[i-1]) && Character.isDigit(tempOrigin[i+1])) {
+                        number += "-"; 
+                    }
+
+                    // Check for duplicates
+                    else if (tempOrigin[i] == tempOrigin[i-1]) {
+                        // Check if subtraction (just in case)
+                        if (tempOrigin[i] == '-') {
+                            result.remove(result.size()-1);
+                            result.add("+");
+                        }
+                        // Check for parenthasis
+                        else if (tempOrigin[i] != '(' && tempOrigin[i] != ')'){
+                            System.out.println("REMOVED DUPLICATE OPERATIONS: " + tempOrigin[i]);
+                            continue;
+                        }
+
+                        // Check for the rest
+                        else {
+                            result.add(Character.toString(tempOrigin[i]));
+                        }
+                    }
+                    else {
+                        // If not, add that token to the result list
+                        result.add(Character.toString(tempOrigin[i]));
+                    }
+                }
+
+                // Checking for other operators that are longer than one character
+                // Checking for sine
+                else if (tempOrigin[i] == 's' && tempOrigin[i+1] == 'i' && tempOrigin[i+2] == 'n') {
+                    result.add("sin");
+                    i += 2;
+                }
+
+                // Checking for cosine
+                else if (tempOrigin[i] == 'c' && tempOrigin[i+1] == 'o' && tempOrigin[i+2] == 's') {
+                    result.add("cos");
+                    i += 2;
+                }
+
+                // Checking for tangent
+                else if (tempOrigin[i] == 't' && tempOrigin[i+1] == 'a' && tempOrigin[i+2] == 'n') {
+                    result.add("tan");
+                    i += 2;
+                }
+
+                // Checking for cotangent
+                else if (tempOrigin[i] == 'c' && tempOrigin[i+1] == 'o' && tempOrigin[i+2] == 't') {
+                    result.add("cot");
+                    i += 2;
+                }
+
+                // Checking for arcsine
+                else if (tempOrigin[i] == 'a' && tempOrigin[i+1] == 'r' && tempOrigin[i+2] == 'c'
+                    && tempOrigin[i+3] == 's' && tempOrigin[i+4] == 'i' && tempOrigin[i+5] == 'n') {
+                    result.add("arcsin");
+                    i += 5;
+                }
+
+                // Checking for arccosine
+                else if (tempOrigin[i] == 'a' && tempOrigin[i+1] == 'r' && tempOrigin[i+2] == 'c'
+                    && tempOrigin[i+3] == 'c' && tempOrigin[i+4] == 'o' && tempOrigin[i+5] == 's') {
+                    result.add("arccos");
+                    i += 5;
+                }
+
+                // Checking for arctangent
+                else if (tempOrigin[i] == 'a' && tempOrigin[i+1] == 'r' && tempOrigin[i+2] == 'c'
+                    && tempOrigin[i+3] == 't' && tempOrigin[i+4] == 'a' && tempOrigin[i+5] == 'n') {
+                    result.add("arctan");
+                    i += 5;
+                }
+
+                // Checking for arccotangent
+                else if (tempOrigin[i] == 'a' && tempOrigin[i+1] == 'r' && tempOrigin[i+2] == 'c'
+                    && tempOrigin[i+3] == 'c' && tempOrigin[i+4] == 't' && tempOrigin[i+5] == 'g') {
+                    result.add("arcctg");
+                    i += 5;
+                }
+
+                // Checking for natural log
+                else if (tempOrigin[i] == 'l' && tempOrigin[i+1] == 'n') {
+                    result.add("ln");
+                    i++;
+                }
+
+                // Checking for log base 10
+                else if (tempOrigin[i] == 'l' && tempOrigin[i+1] == 'o' && tempOrigin[i+2] == 'g') {
+                    result.add("log");
+                    i += 2;
+                }
+
+                // If char does not apply to the expression rules, then show error
+                else {
+                    System.out.println("THERE WAS AN ERROR PROCESSING YOUR EQUATION HERE: " + tempOrigin[i] + " is not a valid expression.");
+                    return null;
+                }
+            }
+            // Catch any errors in expression
+            catch (Exception e) {
+                System.out.println("ERROR: " + tempOrigin[i]);
+            }
+        }
+        return result;
+    }
+}
